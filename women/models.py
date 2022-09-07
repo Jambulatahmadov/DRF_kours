9896e67ffd5dfeb795b5f31ec3c3c5e77c993b26
djambulat <jambulatahmadov99@gmail.com>
2022-09-07 19:42:23 +0300

    first commit

---
+++ women/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Women(models.Model):
+    title = models.CharField(max_length=250,)
+    # slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    content = models.TextField(blank=True)
+    # photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+    # slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+
+    def __str__(self):
+        return self.name
