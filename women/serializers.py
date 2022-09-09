1cf38fe6ea1f4541dc69ecc0b9a214e79e9e98f3
djambulat <jambulatahmadov99@gmail.com>
2022-09-09 16:17:45 +0300

    updated my code & error correction

--- women/serializers.py
+++ women/serializers.py
@@ -5,7 +5,7 @@ from .models import Women
 
 
 class WomenModel():
-    def __int__(self, title, content):
+    def __init__(self, title, content):
         self.title = title
         self.content = content
 
@@ -14,9 +14,12 @@ class WomenSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     content = serializers.CharField()
 
+
 def encode():
     model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
     model_sr = WomenSerializer(model)
-    print(model_sr.data, type(model_sr.date), sep='\n')
-    json = JSONRenderer().render(model_sr.date)
+    print(model_sr.data, type(model_sr.data), sep='\n')
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
+
 
