5509e0f52d2551161c43ea9e72e5ec3b6b9cdb75
djambulat <jambulatahmadov99@gmail.com>
2022-09-09 16:38:37 +0300

    created method decode in serializers.py

--- women/serializers.py
+++ women/serializers.py
@@ -1,4 +1,7 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 
 from .models import Women
@@ -23,3 +26,9 @@ def encode():
     print(json)
 
 
+def decode():
+    stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Content: Angelina Jolie"}')
+    data = JSONParser().parse(stream)
+    serializer = WomenSerializer(data=data)
+    serializer.is_valid()
+    print(serializer.validated_data)
