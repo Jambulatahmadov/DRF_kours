c75547b2810a4bf22b3269cbcdd2c921e4da5054
djambulat <jambulatahmadov99@gmail.com>
2022-09-09 16:01:24 +0300

    created new serializer class

--- women/serializers.py
+++ women/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+class WomenModel():
+    def __int__(self, title, content):
+        self.title = title
+        self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+def encode():
+    model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
+    model_sr = WomenSerializer(model)
+    print(model_sr.data, type(model_sr.date), sep='\n')
+    json = JSONRenderer().render(model_sr.date)
+
