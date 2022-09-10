f81d4e392439b9fec1989a601130d3412c1319dc
djambulat <jambulatahmadov99@gmail.com>
2022-09-10 18:25:20 +0300

    created new url adress

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/womenlist/', WomenAPIList.as_view()),
     path('api/v1/womenlist/<int:pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
